1d3cc51276549aeada55aec8b39cf9a57aa61a8d
shaghi <mojtaba.shagi@gmail.com>
2025-10-14 14:17:00 +0330

    first commit

---
+++ libs/components/src/lib/card/Index.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Index.vue',
+});
+</script>
+
+<template>Card....</template>
+
+<style scoped></style>
